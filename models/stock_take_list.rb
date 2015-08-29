655b593a24f5855651a7fd91a0e6a3b3b9c72c0d
cfaivre <christophe@faivre.co.za>
2015-08-29 17:59:57 +0200

    Sort stock takes more

--- models/stock_take_list.rb
+++ models/stock_take_list.rb
@@ -7,7 +7,13 @@ class StockTakeListModel
   def load_stock_takes
     viewmodel = StockTakeListViewModel.new
     stock_takes = StockApiClient.new.get_stock_takes
-    stock_takes.sort_by! { |stock_take| stock_take["created_at"] } unless stock_takes.empty?
+    stock_takes.sort_by! { |stock_take| stock_take[:created_at] } unless stock_takes.empty?
+    stock_takes.each{|stock_take|
+      stock_take[:created_at] = Time.parse(stock_take[:created_at]).strftime('%Y/%m/%d %H:%M')
+      # Sort stats by biggest count to smallest
+      new_stats = stock_take[:stats].sort_by(&:last).reverse.to_h
+      stock_take[:stats] = new_stats
+    }
     viewmodel.stock_takes = stock_takes
     viewmodel
   end
