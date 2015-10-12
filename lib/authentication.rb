bdb056cb3c442ed424e51f53b218bb7628d1adcb
cfaivre <christophe@faivre.co.za>
2015-10-12 23:17:43 +0200

    Wording changes

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -2,7 +2,7 @@ module Authentication
   def authenticate!
     unless session[:user]
       session[:original_request] = request.path_info
-      flash[:notice] = 'You need to be logged in.'
+      #flash[:notice] = 'You need to be logged in.'
       redirect '/login'
     end
   end
